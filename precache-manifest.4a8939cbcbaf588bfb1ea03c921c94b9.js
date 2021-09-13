4cb1da40eca0efa040e69b1aa5c07cfa38c69cf8
Santiago Alvarez <aalvarez3570@gmail.com>
2021-09-12 22:42:07 -0500

    new Deploy

---
+++ precache-manifest.4a8939cbcbaf588bfb1ea03c921c94b9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a05b17cac6e750016a",
+    "url": "/alvarez-abogados/css/app.c8480868.css"
+  },
+  {
+    "revision": "0e1a0060ce69d74c37a0c16f969e93b4",
+    "url": "/alvarez-abogados/home.json"
+  },
+  {
+    "revision": "efee5b76e4615873a7c8594f7eb477d3",
+    "url": "/alvarez-abogados/img/black-alvarez-bufete-de-abogados.efee5b76.svg"
+  },
+  {
+    "revision": "43db952100f8acd8656a4c1ca617a1b6",
+    "url": "/alvarez-abogados/img/burger-menu-black.43db9521.svg"
+  },
+  {
+    "revision": "db0cba4e8ed571e8a3c053ed4372e98b",
+    "url": "/alvarez-abogados/img/exit-black.db0cba4e.svg"
+  },
+  {
+    "revision": "570a0b262820e988c2b2a85e3a4813d0",
+    "url": "/alvarez-abogados/img/logo-800x640.570a0b26.svg"
+  },
+  {
+    "revision": "2418f0ee32de5709ad627d60f6f9f447",
+    "url": "/alvarez-abogados/img/logo-horizontal-black-alvarez-bufete-de-abogados.2418f0ee.svg"
+  },
+  {
+    "revision": "18aac89645039a89851b2cc61982c82c",
+    "url": "/alvarez-abogados/img/see-more.18aac896.svg"
+  },
+  {
+    "revision": "6e9298b9539b8f1203b139008bf820f2",
+    "url": "/alvarez-abogados/index.html"
+  },
+  {
+    "revision": "ccdc2dee805e389d6e36",
+    "url": "/alvarez-abogados/js/about.85369cd4.js"
+  },
+  {
+    "revision": "84a05b17cac6e750016a",
+    "url": "/alvarez-abogados/js/app.c1112633.js"
+  },
+  {
+    "revision": "06f857c82aebd595efd5",
+    "url": "/alvarez-abogados/js/chunk-vendors.850563f6.js"
+  },
+  {
+    "revision": "6a680603b0ea2030805d561cf15016d6",
+    "url": "/alvarez-abogados/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/alvarez-abogados/robots.txt"
+  }
+]);
