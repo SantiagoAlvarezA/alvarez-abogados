ef348f6783246263cc4b571c29a323b99d5a4d70
Santiago Alvarez <aalvarez3570@gmail.com>
2021-09-12 22:41:44 -0500

    se agrego texto en quiees somos y que hacemos

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>about</h1>
+  <h1 class="text-left px-3 text-black" >¿QUIENES SOMOS?</h1>
+  <p class="text text-justify text-gray-500 px-3 my-5" >
+    Un grupo selecto de los mejores abogados en las diferentes áreas, con asesoría y asistencia legal en todas las áreas del derecho,  contando con profesionales graduados de las mejores universidades, además han realizado estudios de postgrado y especialización en cada una de las áreas que prestamos el servicio.
+    También contamos con profesionales auxiliares:
+    Peritos inmobiliarios: Profesionales adscritos con amplia experiencia dentro de la región, en cuanto avalúos, presentación de informes de peritaje.
+    Psicólogos: profesionales especializados que brindan un acompañamiento tanto a nuestros clientes en los momentos de ser requeridos, entendiendo que merecen un servicio integral, como apoyo a nuestros profesionales con el fin de prestar un servicio de calidad siempre enfocados en el lado ético y moral.
+
+    Contadores: Contamos con profesionales que contribuyen a esclarecer temas contables, declaraciones fiscales y apoyo en la materia cuando sea necesario.
+
+    Además contamos con personal auxiliar conocedores en temas de inversión, que contribuyen al tema de inversiones tanto en bienes muebles, inmuebles y el mundo virtual de inversión en la bolsa de valores.
+  </p>
 </template>
--- src/views/WhatWeDo.vue
+++ src/views/WhatWeDo.vue
@@ -1,3 +1,6 @@
 <template>
-    <h1>what-we-do</h1>
+    <h1 class="text-left px-3 text-black ">¿Qué hacemos?</h1>
+    <p class="text text-justify text-gray-500 px-3 my-5">        
+        Nuestro objetivo es brindar de la mano con usted y hacer su vida mas fácil, colocando a su disposición nuestros profesionales con sus conocimientos y experiencia con el fin de disminuir o resolver los riesgos jurídicos
+    </p>
 </template>
